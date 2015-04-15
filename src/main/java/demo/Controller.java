3fd669aaf4fc227920a5374c37197b471252928a
Fredrik Löfgren <fredrik.lofgren@omegapoint.se>
2015-04-15 07:13:22 +0200

    Added the ability to vote on something, record the result in memory and display the results.

--- src/main/java/demo/Controller.java
+++ src/main/java/demo/Controller.java
@@ -1,4 +1,40 @@
 package demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class Controller {
+
+    private final Map<String, Integer> voteCount;
+
+    public Controller() {
+        this.voteCount = new HashMap<>();
+    }
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/result")
+    public Map<String, Integer> getResult() {
+        return voteCount;
+    }
+
+    @RequestMapping("/vote")
+    public String placeVote(@RequestParam final String alternative) {
+        if (alternative == null || alternative.trim().equals("")) {
+            return "Ogiltigt alternativ";
+        }
+        Integer count = voteCount.get(alternative);
+        voteCount.put(alternative, count == null ? 1 : count + 1);
+        return String.format("Your vote has been placed on %s", alternative);
+    }
+
+
 }
